1e9578536d5fff92ee196b3c8cfff29460689de0
Spacemixup <xchrislee@gmail.com>
2015-10-25 04:07:22 -0700

    ian

--- collection.js
+++ collection.js
@@ -1,5 +1,4 @@
 Markers = new Mongo.Collection('markers');
-Merchants = new Mongo.Collection('merchant');
 Customers = new Mongo.Collection('customers');
 MerchantTransactions = new Mongo.Collection('merchanttransactions')
 CustomerTransactions = new Mongo.Collection('customertransactions')
