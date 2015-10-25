56f64508e1856f1b022d164d83cd9589a37ddbcb
Spacemixup <xchrislee@gmail.com>
2015-10-25 08:34:31 -0700

    Dynamic updating of confirm view.

--- client/confirm.js
+++ client/confirm.js
@@ -3,3 +3,17 @@ Template.confirm.events({
     Router.go('/');
   }
 })
+Template.confirm.helpers({
+	totalCharge: function(){
+		var amount = CustomerRequests.findOne();
+		return amount.amount + 2;
+	},
+	serviceFee: function(){
+		return 2;
+	},
+	cashAmount: function(){
+		var amount = CustomerRequests.findOne();
+		return amount.amount;
+	}
+
+})
